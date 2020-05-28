fce55d483f51ea6cbb1a251b11983b877ca3b513
Ireallyhateyou <52547160+Ireallyhateyou@users.noreply.github.com>
2020-05-28 02:02:51 +0200

    last

---
+++ Nier_Mini_Game - Copie/Assets/Scripts/Cube_Black_hit_Fade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Black_hit_Fade : MonoBehaviour
+{
+    public Animator HitAnimation;
+    private bool NotTooLong = false ;
+    private float timestamp;
+    public float Time_Between_Hits = .01f;
+    void Start()
+    {
+        HitAnimation = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            HitAnimation.SetTrigger("IsHit");
+            timestamp = Time.time + Time_Between_Hits;
+            HitAnimation.SetTrigger("StopHittingOnME");
+        }
+
+        if (Time.time >= timestamp)
+        {
+            
+   
+        }
+
+        
+    }
+}
---
+++ Nier_Mini_Game - Copie/Assets/Scripts/Player_Life_Cube2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Life_Cube2 : MonoBehaviour
+{
+    void Update()
+    {
+        if (Player_Life.Life == 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
